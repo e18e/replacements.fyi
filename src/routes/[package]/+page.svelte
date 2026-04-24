2ffed5bb655eabccdd1c9fce5fbededd7f646530
Alexander Karan <tech@alexanderkaran.com>
2026-04-24 13:40:06 +0800

    Rename site

--- src/routes/[package]/+page.svelte
+++ src/routes/[package]/+page.svelte
@@ -2,7 +2,7 @@
 	import { resolve } from '$app/paths';
 	import { all, type EngineConstraint, type KnownUrl } from 'module-replacements';
 	import { highlight } from './highlight.remote';
-	import MrE18e from '$lib/MrE18e.svelte';
+	import ReplacementsTitle from '$lib/ReplacementsTitle.svelte';
 
 	let { params } = $props();
 
@@ -71,7 +71,7 @@
 	{/if}
 </svelte:head>
 
-<a href={resolve('/')} class="back-link"><MrE18e /></a>
+<a href={resolve('/')} class="back-link"><ReplacementsTitle /></a>
 <div class="page">
 	{#if !mapping}
 		<div class="not-found">
