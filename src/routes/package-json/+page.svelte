8bfa8560d550f592dfd184676b973925e5fdbf2f
Alexander Karan <tech@alexanderkaran.com>
2026-05-19 18:10:42 +0800

    Add submit button

--- src/routes/package-json/+page.svelte
+++ src/routes/package-json/+page.svelte
@@ -125,47 +125,47 @@
 	</header>
 
 	<form {...scan_package_json_file} enctype="multipart/form-data">
-		<FileInput
-			name="package_json"
-			accept="application/json,.json"
-			placeholder={!file_name}
-			selected={Boolean(file_name)}
-			aria-label="Upload package.json"
-			onchange={handle_file_change}
-		>
-			<span class="file-input-prompt">
-				<span>Drag file or</span>
-				<span class="file-input-action">Select Here</span>
-			</span>
-			{#if file_name}
-				<span class="file-input-selection">
-					<svg
-						class="file-input-icon"
-						xmlns="http://www.w3.org/2000/svg"
-						width="16"
-						height="16"
-						fill="currentColor"
-						viewBox="0 0 16 16"
-						aria-hidden="true"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M14 4.5V11h-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zM4.151 15.29a1.2 1.2 0 0 1-.111-.449h.764a.58.58 0 0 0 .255.384q.105.073.25.114.142.041.319.041.245 0 .413-.07a.56.56 0 0 0 .255-.193.5.5 0 0 0 .084-.29.39.39 0 0 0-.152-.326q-.152-.12-.463-.193l-.618-.143a1.7 1.7 0 0 1-.539-.214 1 1 0 0 1-.352-.367 1.1 1.1 0 0 1-.123-.524q0-.366.19-.639.192-.272.528-.422.337-.15.777-.149.456 0 .779.152.326.153.5.41.18.255.2.566h-.75a.56.56 0 0 0-.12-.258.6.6 0 0 0-.246-.181.9.9 0 0 0-.37-.068q-.324 0-.512.152a.47.47 0 0 0-.185.384q0 .18.144.3a1 1 0 0 0 .404.175l.621.143q.326.075.566.211a1 1 0 0 1 .375.358q.135.222.135.56 0 .37-.188.656a1.2 1.2 0 0 1-.539.439q-.351.158-.858.158-.381 0-.665-.09a1.4 1.4 0 0 1-.478-.252 1.1 1.1 0 0 1-.29-.375m-3.104-.033a1.3 1.3 0 0 1-.082-.466h.764a.6.6 0 0 0 .074.27.5.5 0 0 0 .454.246q.285 0 .422-.164.137-.165.137-.466v-2.745h.791v2.725q0 .66-.357 1.005-.355.345-.985.345a1.6 1.6 0 0 1-.568-.094 1.15 1.15 0 0 1-.407-.266 1.1 1.1 0 0 1-.243-.39m9.091-1.585v.522q0 .384-.117.641a.86.86 0 0 1-.322.387.9.9 0 0 1-.47.126.9.9 0 0 1-.47-.126.87.87 0 0 1-.32-.387 1.55 1.55 0 0 1-.117-.641v-.522q0-.386.117-.641a.87.87 0 0 1 .32-.387.87.87 0 0 1 .47-.129q.265 0 .47.129a.86.86 0 0 1 .322.387q.117.255.117.641m.803.519v-.513q0-.565-.205-.973a1.46 1.46 0 0 0-.59-.63q-.38-.22-.916-.22-.534 0-.92.22a1.44 1.44 0 0 0-.589.628q-.205.407-.205.975v.513q0 .562.205.973.205.407.589.626.386.217.92.217.536 0 .917-.217.384-.22.589-.626.204-.41.205-.973m1.29-.935v2.675h-.746v-3.999h.662l1.752 2.66h.032v-2.66h.75v4h-.656l-1.761-2.676z"
-						/>
-					</svg>
-					<span class="file-input-name">{file_name}</span>
+		<div class="package-json-upload">
+			<FileInput
+				name="package_json"
+				accept="application/json,.json"
+				placeholder={!file_name}
+				selected={Boolean(file_name)}
+				aria-label="Upload package.json"
+				onchange={handle_file_change}
+			>
+				<span class="file-input-prompt">
+					<span>Drag file or</span>
+					<span class="file-input-action">Select Here</span>
 				</span>
-			{/if}
-		</FileInput>
+				{#if file_name}
+					<span class="file-input-selection">
+						<svg
+							class="file-input-icon"
+							xmlns="http://www.w3.org/2000/svg"
+							width="16"
+							height="16"
+							fill="currentColor"
+							viewBox="0 0 16 16"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M14 4.5V11h-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zM4.151 15.29a1.2 1.2 0 0 1-.111-.449h.764a.58.58 0 0 0 .255.384q.105.073.25.114.142.041.319.041.245 0 .413-.07a.56.56 0 0 0 .255-.193.5.5 0 0 0 .084-.29.39.39 0 0 0-.152-.326q-.152-.12-.463-.193l-.618-.143a1.7 1.7 0 0 1-.539-.214 1 1 0 0 1-.352-.367 1.1 1.1 0 0 1-.123-.524q0-.366.19-.639.192-.272.528-.422.337-.15.777-.149.456 0 .779.152.326.153.5.41.18.255.2.566h-.75a.56.56 0 0 0-.12-.258.6.6 0 0 0-.246-.181.9.9 0 0 0-.37-.068q-.324 0-.512.152a.47.47 0 0 0-.185.384q0 .18.144.3a1 1 0 0 0 .404.175l.621.143q.326.075.566.211a1 1 0 0 1 .375.358q.135.222.135.56 0 .37-.188.656a1.2 1.2 0 0 1-.539.439q-.351.158-.858.158-.381 0-.665-.09a1.4 1.4 0 0 1-.478-.252 1.1 1.1 0 0 1-.29-.375m-3.104-.033a1.3 1.3 0 0 1-.082-.466h.764a.6.6 0 0 0 .074.27.5.5 0 0 0 .454.246q.285 0 .422-.164.137-.165.137-.466v-2.745h.791v2.725q0 .66-.357 1.005-.355.345-.985.345a1.6 1.6 0 0 1-.568-.094 1.15 1.15 0 0 1-.407-.266 1.1 1.1 0 0 1-.243-.39m9.091-1.585v.522q0 .384-.117.641a.86.86 0 0 1-.322.387.9.9 0 0 1-.47.126.9.9 0 0 1-.47-.126.87.87 0 0 1-.32-.387 1.55 1.55 0 0 1-.117-.641v-.522q0-.386.117-.641a.87.87 0 0 1 .32-.387.87.87 0 0 1 .47-.129q.265 0 .47.129a.86.86 0 0 1 .322.387q.117.255.117.641m.803.519v-.513q0-.565-.205-.973a1.46 1.46 0 0 0-.59-.63q-.38-.22-.916-.22-.534 0-.92.22a1.44 1.44 0 0 0-.589.628q-.205.407-.205.975v.513q0 .562.205.973.205.407.589.626.386.217.92.217.536 0 .917-.217.384-.22.589-.626.204-.41.205-.973m1.29-.935v2.675h-.746v-3.999h.662l1.752 2.66h.032v-2.66h.75v4h-.656l-1.761-2.676z"
+							/>
+						</svg>
+						<span class="file-input-name">{file_name}</span>
+					</span>
+				{/if}
+			</FileInput>
+			<button class="no-js-submit" type="submit">Scan package.json</button>
+		</div>
 		{#if scan_error}
 			<p class="error" role="alert">
 				<span class="error-label">// error</span>
 				<span>{scan_error}</span>
 			</p>
 		{/if}
-		<span class="no-js-submit-row">
-			<button class="no-js-submit" type="submit">Scan package.json</button>
-		</span>
 	</form>
 
 	<PackageJsonPasteStatus pasted={pasted_successfully} />
@@ -289,13 +289,24 @@
 		margin: 0;
 	}
 
-	.no-js-submit-row {
-		display: flex;
-		justify-content: flex-end;
-		margin: 0 0 1.5rem;
+	.package-json-upload {
+		position: relative;
+		margin-bottom: 1.5rem;
+	}
+
+	.package-json-upload :global(.file-input) {
+		margin-bottom: 0;
+	}
+
+	.package-json-upload :global(.file-input-content) {
+		padding-bottom: 2.9rem;
 	}
 
 	.no-js-submit {
+		position: absolute;
+		right: 0.75rem;
+		bottom: 0.75rem;
+		z-index: 1;
 		display: inline-flex;
 		align-items: center;
 		justify-content: center;
@@ -320,6 +331,11 @@
 		background: color-mix(in srgb, var(--accent) 15%, transparent);
 		border-color: var(--border);
 		color: var(--accent-contrast);
+	}
+
+	.no-js-submit:focus-visible {
+		outline: 2px solid var(--accent);
+		outline-offset: 2px;
 	}
 
 	.error {
